f75aa21c43fae7953e036dd8a0caed16edd766a5
Romain Neutron <imprec@gmail.com>
2015-01-12 11:42:41 +0100

    Fix recipes encoding

--- Formula/blackfire-agent.rb
+++ Formula/blackfire-agent.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'formula'
 
 class BlackfireAgent < Formula
--- Formula/blackfire-php55.rb
+++ Formula/blackfire-php55.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require File.expand_path("../../Abstract/abstract-blackfire-php-extension", __FILE__)
 
 class BlackfirePhp55 < AbstractBlackfirePhpExtension
