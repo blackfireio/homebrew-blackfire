d19a143aef71b4ce84359c15b257af8994cd79b7
Lucas Michot <lucas@semalead.com>
2015-07-01 13:03:33 +0200

    Just extend AbstractBlackfirePhpExtension

--- Formula/blackfire-php55.rb
+++ Formula/blackfire-php55.rb
@@ -4,25 +4,4 @@ require File.expand_path("../../Abstract/abstract-blackfire-php-extension", __FI
 
 class BlackfirePhp55 < AbstractBlackfirePhpExtension
     init
-    homepage "https://blackfire.io"
-    version '0.24.2'
-
-    url 'http://packages.blackfire.io/homebrew/blackfire-php_0.24.2-darwin_amd64-php55.tar.gz'
-    sha1 '0ab3525d3d46464929f7d86410a9f4126e9472ab'
-
-    def install
-        prefix.install "blackfire.so"
-        write_config_file
-    end
-
-    def config_file
-        super + <<-EOS.undent
-        blackfire.agent_socket = unix:///usr/local/var/run/blackfire-agent.sock
-        blackfire.agent_timeout = 0.25
-        ;blackfire.log_level = 3
-        ;blackfire.log_file = /tmp/blackfire.log
-        ;blackfire.server_id =
-        ;blackfire.server_token =
-        EOS
-    end
 end
--- Formula/blackfire-php56.rb
+++ Formula/blackfire-php56.rb
@@ -4,25 +4,4 @@ require File.expand_path("../../Abstract/abstract-blackfire-php-extension", __FI
 
 class BlackfirePhp56 < AbstractBlackfirePhpExtension
     init
-    homepage "https://blackfire.io"
-    version '0.24.2'
-
-    url 'http://packages.blackfire.io/homebrew/blackfire-php_0.24.2-darwin_amd64-php56.tar.gz'
-    sha1 '78cc08ddd295c30c24e0f9c127cef9f8acf3e1d6'
-
-    def install
-        prefix.install "blackfire.so"
-        write_config_file
-    end
-
-    def config_file
-        super + <<-EOS.undent
-        blackfire.agent_socket = unix:///usr/local/var/run/blackfire-agent.sock
-        blackfire.agent_timeout = 0.25
-        ;blackfire.log_level = 3
-        ;blackfire.log_file = /tmp/blackfire.log
-        ;blackfire.server_id =
-        ;blackfire.server_token =
-        EOS
-    end
 end
