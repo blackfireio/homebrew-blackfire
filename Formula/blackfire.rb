15b155479784e56a08a39527849d8d07fabce4e0
Continuous integration <sysadmin@blackfire.io>
2026-07-10 15:18:29 +0000

    Update Blackfire formula to 2026.7.0

--- Formula/blackfire.rb
+++ Formula/blackfire.rb
@@ -4,14 +4,14 @@ require 'formula'
 
 class Blackfire < Formula
     homepage 'https://blackfire.io'
-    version '2026.6.1'
+    version '2026.7.0'
 
     if Hardware::CPU.arm?
-        url 'https://packages.blackfire.io/blackfire/2026.6.1/blackfire-darwin_arm64.pkg.tar.gz'
-        sha256 '39fb3dada02dc7f76e4bc75759fbe3286ce126bde3f7e8241fc94ffd52df9e41'
+        url 'https://packages.blackfire.io/blackfire/2026.7.0/blackfire-darwin_arm64.pkg.tar.gz'
+        sha256 'c735b0eaeb3ef7fafc94c307653804fbead7ed96528402fb2fb7ce9de00b0384'
     else
-        url 'https://packages.blackfire.io/blackfire/2026.6.1/blackfire-darwin_amd64.pkg.tar.gz'
-        sha256 'fab3220ffbc514803c751deab949e9afcb8335376f9d7c988d3f1bae088ec412'
+        url 'https://packages.blackfire.io/blackfire/2026.7.0/blackfire-darwin_amd64.pkg.tar.gz'
+        sha256 'd279f35581780148c922e33d8d2b5006eaa8f119b66ba063bf2a118c78da7a84'
     end
 
     conflicts_with "blackfire-agent", because: "blackfire replaces the blackfire-agent package"
