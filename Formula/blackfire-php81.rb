511b56bf96a5fc3fcdc17294c692e358e4a9bd90
Continuous Integration <sysadmin@blackfire.io>
2026-08-20 10:21:59 +0000

    Update Blackfire formulae to 2026.8.4

--- Formula/blackfire-php81.rb
+++ Formula/blackfire-php81.rb
@@ -5,13 +5,13 @@ require File.expand_path("../../Abstract/abstract-blackfire-php-extension", __FI
 class BlackfirePhp81 < AbstractBlackfirePhpExtension
     init
     homepage "https://blackfire.io"
-    version '2026.8.3'
+    version '2026.8.4'
 
     if Hardware::CPU.arm?
-        url 'https://packages.blackfire.io/homebrew/blackfire-php_2026.8.3-darwin_arm64-php81.tar.gz'
+        url 'https://packages.blackfire.io/homebrew/blackfire-php_2026.8.4-darwin_arm64-php81.tar.gz'
         sha256 ''
     else
-        url 'https://packages.blackfire.io/homebrew/blackfire-php_2026.8.3-darwin_amd64-php81.tar.gz'
+        url 'https://packages.blackfire.io/homebrew/blackfire-php_2026.8.4-darwin_amd64-php81.tar.gz'
         sha256 ''
     end
 
